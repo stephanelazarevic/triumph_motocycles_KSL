0e26618a21f8fda811e5e64cd15d3368aca5b8a5
Stephane LAZAREVIC <stephanelazarevic042@outlook.com>
2025-02-05 17:23:59 +0100

    fix review

--- infrastructure/platforms/deno/schemas/rentalRequestSchema.ts
+++ infrastructure/platforms/deno/schemas/rentalRequestSchema.ts
@@ -4,11 +4,9 @@ export const addRentalRequestSchema = z.object({
  clientId: z.string().uuid("Invalid UUID format"),
  motorcycleId: z.string().uuid("Invalid UUID format"),   
  startDate: z.coerce.date()
-   .min(new Date(1900, 0, 1), "Date must be after 1900")
-   .max(new Date(), "Date cannot be in the future"),
+   .min(new Date(1900, 0, 1), "Date must be after 1900"),
  endDate: z.coerce.date()
-   .min(new Date(1900, 0, 1), "Date must be after 1900")
-   .max(new Date(), "Date cannot be in the future"),
+   .min(new Date(1900, 0, 1), "Date must be after 1900"),
  cost: z.number().int(),
  isCompleted: z.boolean(),
 })
