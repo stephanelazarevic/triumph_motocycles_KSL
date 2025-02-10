eb0c99f406b4af8082823d605df73e6a5cc1f76f
Lucie <lucie.godard1704@gmail.com>
2025-02-10 03:41:02 +0100

    refacto remove olf files oups

--- infrastructure/client/nextjs/src/app/admin/stock/page.tsx
+++
@@ -1,9 +0,0 @@
-import AdminStock from '@/components/AdminStock';
-
-export default function AdminEssai() {
-  return (
-    <main className="min-h-screen bg-gray-50">
-      <AdminStock />
-    </main>
-  );
-}
