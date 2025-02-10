5f65eb0ab90bbe7b145c38524c561cbb99c5fc53
Lucie <lucie.godard1704@gmail.com>
2025-02-10 05:11:17 +0100

    feat(motorcycle): update routes

--- interface/api/deno/main.ts
+++ interface/api/deno/main.ts
@@ -1,8 +1,11 @@
 import { prisma } from "./config/prisma.db.ts";
 import apiRouter from "./routes/apiRouter.ts";
 import authenticationRouter from "./routes/authenticationRouter.ts";
-
+import motorcycleRouter from "./routes/motorcycleRouter.ts";
 import { sendNotificationsCron, retryFailedNotificationsCron } from "../deno/config/cronDependencies.ts";
+import { logger } from './middleware/logger.ts'
+import { Hono } from "https://deno.land/x/hono@v3.11.4/mod.ts";
+import { cors } from "https://deno.land/x/hono@v3.11.4/middleware.ts";
 
 const app = new Hono();
 
@@ -32,6 +35,7 @@ app.use("*", logger);
 // routes
 app.route("/api", apiRouter);
 app.route("/api", authenticationRouter);
+app.route("/api/motorcycle", motorcycleRouter);
 
 // Start server
 Deno.serve({ port: 8000 }, app.fetch);
