1342db232b6db8ea4800a1b0d82ee5d0a6071692
Lucie <lucie.godard1704@gmail.com>
2025-02-10 02:16:19 +0100

    feat(auth): Hono connexion successful and login OK

--- infrastructure/client/vue/src/main.ts
+++ infrastructure/client/vue/src/main.ts
@@ -7,7 +7,7 @@ import { router } from './router/router';
 import axios from 'axios';
 import { setupAxiosInterceptors } from './utils/interceptors';
 
-axios.defaults.baseURL = 'http://localhost:8000';
+axios.defaults.baseURL = 'http://127.0.0.1:8000';
 setupAxiosInterceptors();
 
 const app = createApp(App);
--- infrastructure/platforms/deno/routes/authenticationRouter.ts
+++ infrastructure/platforms/deno/routes/authenticationRouter.ts
@@ -1,14 +1,12 @@
-import { Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
+import { Hono } from "https://deno.land/x/hono@v3.11.4/mod.ts";
 import { AuthenticationController } from "../controllers/AuthenticationController.ts";
 import { userRepository } from "../config/repositoriesDependencies.ts";
 import { passwordService, tokenGeneratorService } from "../config/servicesDependencies.ts";
 
-import { handleRequest } from "../utils/handleRequest.ts";
-
-const authenticationRouter = new Router();
+const authenticationRouter = new Hono();
 
 const authenticationController = new AuthenticationController(userRepository, passwordService, tokenGeneratorService);
 
-authenticationRouter.post("/api/signin", handleRequest(authenticationController.signIn.bind(authenticationController)));
+authenticationRouter.post("/login", (context) => authenticationController.signIn(context))
 
 export default authenticationRouter;
